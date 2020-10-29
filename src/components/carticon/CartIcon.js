190f82268d1e00471874daaecd02a7c3034a53e4
Paula Licausi <paulicausi@gmail.com>
2020-10-29 13:28:37 -0300

    CartIcon component

---
+++ src/components/carticon/CartIcon.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from '../../assets/img/logo.svg';
+
+const CartIcon = () => {
+   return (
+    <img src={logo} alt="logo" />
+   );
+}
+
+export default CartIcon;
