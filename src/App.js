15052eb47ce27b5a543b4453b4847b638c597280
Paula Licausi <paulicausi@gmail.com>
2020-11-16 19:37:18 -0300

    App font and containers

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Home from './containers/home/Home';
 import NavBar from './components/navbar/NavBar';
 import Footer from './components/footer/Footer';
+import ItemDetailContainer from './containers/itemDetailContainer/ItemDetailContainer';
 
 function App() {
   return (
       <div>
         <NavBar />
-        <Home greeting="Hola! :)" />
+        <Home greeting="¡¡¡¡Bienvenid@s!!!!" />
+        <ItemDetailContainer />
         <Footer />
       </div>
   );
