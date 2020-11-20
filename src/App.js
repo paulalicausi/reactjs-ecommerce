bb6eac74cb3747980eba1edb0230c7ddfa921ae6
Paula Licausi <paulicausi@gmail.com>
2020-11-20 20:30:27 -0300

    Set up routing

--- src/App.js
+++ src/App.js
@@ -3,15 +3,28 @@ import Home from './containers/home/Home';
 import NavBar from './components/navbar/NavBar';
 import Footer from './components/footer/Footer';
 import ItemDetailContainer from './containers/itemDetailContainer/ItemDetailContainer';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Cart from './components/cart/Cart';
+import CartIcon from './components/carticon/CartIcon';
 
 function App() {
   return (
-      <div>
+      <BrowserRouter>
         <NavBar />
-        <Home greeting="¡¡¡¡Bienvenid@s!!!!" />
-        <ItemDetailContainer />
+        <Switch>
+          <Route exact path="/">
+            <Home greeting="¡¡¡¡Bienvenid@s!!!!" />
+          </Route>
+          <Route exact path="/products/:id">
+            <ItemDetailContainer />
+          </Route>
+          <Route exact path="/cart/">
+            <Cart />
+          </Route>
+        </Switch>
+        <CartIcon />
         <Footer />
-      </div>
+      </BrowserRouter>
   );
 }
 
